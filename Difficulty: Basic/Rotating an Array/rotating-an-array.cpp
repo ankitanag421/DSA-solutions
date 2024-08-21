9d0bce7aa593ffa3e2bd9d2d0c9536b1f0e7aaad
Ankita Nag <151819789+ankitanag421@users.noreply.github.com>
2024-08-21 20:43:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotating an Array/rotating-an-array.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    void leftRotate(vector<int>& arr, int d) {
+        // code here
+        
+        int n=arr.size();
+        d=d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+        
+    }
+    void reverse(vector<int>& arr,int i,int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+    while (t--) {
+        vector<int> arr;
+        int d;
+        string input;
+
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        cin >> d;
+        cin.ignore(); // Ignore the newline character after d
+        Solution ob;
+        ob.leftRotate(arr, d);
+        for (int x : arr) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
