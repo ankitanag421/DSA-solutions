f443af15f60c79945d3e4a170c896b55207acef7
Ankita Nag <151819789+ankitanag421@users.noreply.github.com>
2024-08-21 17:53:26 +0530

    Time: 571 ms (20.04%), Space: 284.4 MB (10.52%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+void merge(vector<int>& nums,int low,int mid,int high){
+    int left=low;
+    int right=mid+1;
+    vector<int>temp;
+    while(left<=mid && right<=high){
+        if(nums[left]<=nums[right]){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        else{
+            temp.push_back(nums[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(nums[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(nums[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++){
+        nums[i]=temp[i-low];
+    }
+   
+}
+ void ms(vector<int>& nums,int low,int high){
+    if(low<high){
+    int mid=(low+high)/2;
+    ms(nums,low,mid);
+    ms(nums,mid+1,high);
+    merge(nums,low,mid,high);
+    }
+ }
+    vector<int> sortArray(vector<int>& nums) {
+        ms(nums,0,nums.size()-1);
+        return nums;
+    }
+};
