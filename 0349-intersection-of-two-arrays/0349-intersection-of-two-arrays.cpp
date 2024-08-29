2ac823a208eaf7d74b3d45e364b3ef31eac70c8f
Ankita Nag <151819789+ankitanag421@users.noreply.github.com>
2024-08-29 22:12:30 +0530

    Time: 0 ms (100%), Space: 12.7 MB (84.79%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,25 +3,25 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        unordered_set<int>s;
+        
         int i=0;
         int j=0;
         vector <int> ans;
         while(i<nums1.size() && j<nums2.size() ){
-            if(nums1[i]==nums2[j]){
-              s.insert(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]>nums2[j])
-             j++;
-             else
-                i++;
-        }    
-        for(auto it:s){
-            ans.push_back(it);
+          if(nums1[i]<nums2[j])
+          i++;
+          else if(nums2[j]<nums1[i])
+          j++;
+          else{
+            ans.push_back(nums1[i]);
+            i++;
+            j++;
+
+          }
         }
-        
+        ans.erase(
+            unique(ans.begin(),ans.end()),
+            ans.end());
         return ans;
     }
 };
